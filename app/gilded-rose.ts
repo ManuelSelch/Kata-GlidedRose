ed0dfc7b5384f31cd19e53f2b45a2f420f18a35d
Manuel Selch <manuel@selch.de>
2026-02-15 00:11:50 +0100

    refactor: guard legendary Sulfuras

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -26,11 +26,13 @@ export class GildedRose {
   }
 
   private updateItem(i: number) {
+    // “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+    if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') return
+    
+
     if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (this.items[i].quality > 0) {
-        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          this.items[i].quality -= 1
-        }
+        this.items[i].quality -= 1
       }
     } else {
       if (this.items[i].quality < 50) {
@@ -49,16 +51,14 @@ export class GildedRose {
         }
       }
     }
-    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      this.items[i].sellIn -= 1;
-    }
+    
+    this.items[i].sellIn -= 1;
+
     if (this.items[i].sellIn < 0) {
       if (this.items[i].name != 'Aged Brie') {
         if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (this.items[i].quality > 0) {
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              this.items[i].quality -= 1
-            }
+            this.items[i].quality -= 1
           }
         } else {
           this.items[i].quality = 0
