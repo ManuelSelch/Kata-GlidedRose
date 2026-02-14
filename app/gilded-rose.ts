528f1759cb6a496600117f74f219ccf66d7189ae
Manuel Selch <manuel@selch.de>
2026-02-15 00:05:09 +0100

    refactor: use +=

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -27,16 +27,16 @@ export class GildedRose {
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
+          this.items[i].quality += 1
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i].quality += 1
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i].quality += 1
               }
             }
           }
@@ -58,7 +58,7 @@ export class GildedRose {
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+            this.items[i].quality += 1
           }
         }
       }
